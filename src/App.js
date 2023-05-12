54363a3dcc251ee9ba387960c6fd5e57ff98126a
name-q <1620206666@qq.com>
2023-05-12 17:31:46 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { observable, autorun } from 'mobx'
+var observableNumber = observable.box(10)
+var observableString = observable.box('qy')
+
+autorun(() => {
+  console.log('observableNumber>>', observableNumber.get())
+})
+
+autorun(() => {
+  console.log('observableString>>', observableString.get())
+})
+
+setTimeout(() => {
+  observableNumber.set(20)
+}, 1000)
+setTimeout(() => {
+  observableString.set('name-q')
+}, 2000)
+
+var observableObj = observable.map({
+  name: 'qy',
+  age: 27
+})
+
+autorun(() => {
+  console.log('observableObj>>', observableObj.get('name'))
+})
+setTimeout(() => {
+  observableObj.set('name', 'name-q')
+}, 3000)
+
 function App() {
   return (
     <div className="App">
